e76ef08932636e7428266548a5b6e27a91223b33
Zbigniew Piotrowski <zbigniew.piotrowski@ecmwf.int>
2022-09-01 17:59:22 +0200

    Calling mock-up of full dwarf, no derived types yet

--- src/cloudsc2_nl/cloudsc_driver_mod.F90
+++ src/cloudsc2_nl/cloudsc_driver_mod.F90
@@ -8,7 +8,8 @@
 ! nor does it submit to any jurisdiction.
 !
 MODULE CLOUDSC_DRIVER_MOD
-  USE PARKIND1, ONLY: JPIM, JPIB, JPRB, JPRD
+!  USE PARKIND1, ONLY: JPIM, JPIB, JPRB, JPRD
+  USE PARKIND1, ONLY: JPIM, JPIB, JPRD
   USE YOMPHYDER, ONLY: STATE_TYPE
   USE YOECLDP, ONLY : NCLV, NCLDQL, NCLDQI 
   USE CLOUDSC_MPI_MOD, ONLY: NUMPROC, IRANK
@@ -16,8 +17,121 @@ MODULE CLOUDSC_DRIVER_MOD
   USE EC_PMON_MOD, ONLY: EC_PMON
 
   IMPLICIT NONE
-
+!INTEGER, PARAMETER :: JPRB = SELECTED_REAL_KIND(13,300)
+INTEGER, PARAMETER :: JPRB = 8
 CONTAINS
+
+  SUBROUTINE CLOUDSC_DRIVER_PRINT( &
+     & NUMOMP, NPROMA, NLEV, NGPTOT, NGPTOTG, NBLOCKS)
+    INTEGER(KIND=JPIM), INTENT(IN)    :: NUMOMP, NPROMA, NLEV, NGPTOT, NGPTOTG, NBLOCKS
+  PRINT *,'NPROMA is:',NPROMA
+  END SUBROUTINE CLOUDSC_DRIVER_PRINT
+
+  SUBROUTINE CLOUDSC_DRIVER_TEST( &
+     & NUMOMP, NPROMA, NLEV, NGPTOT, NGPTOTG, NBLOCKS, PTSPHY, &
+     & PT, PQ, &
+!     & TENDENCY_CML, TENDENCY_LOC, &
+     & PAP,      PAPH, &
+     & PLU,      PLUDE,    PMFU,     PMFD, &
+     & PA,       PCLV,     PSUPSAT,&
+     & PCOVPTOT, &
+     & PFPLSL,   PFPLSN,   PFHPSL,   PFHPSN &
+     & )
+    ! Driver routine that performans the parallel NPROMA-blocking and
+    ! invokes the CLOUDSC2 kernel
+
+    INTEGER(KIND=JPIM), INTENT(IN)    :: NUMOMP, NPROMA, NLEV, NGPTOT, NGPTOTG, NBLOCKS
+    REAL(KIND=JPRB),    INTENT(IN)    :: PTSPHY       ! Physics timestep
+    REAL(KIND=JPRB),    INTENT(IN)    :: PT(:,:,:)    ! T at start of callpar
+    REAL(KIND=JPRB),    INTENT(IN)    :: PQ(:,:,:)    ! Q at start of callpar
+!    TYPE(STATE_TYPE),   INTENT(IN)    :: TENDENCY_CML(10) ! cumulative tendency used for final output
+!    TYPE(STATE_TYPE),   INTENT(OUT)   :: TENDENCY_LOC(10) ! local tendency from cloud scheme
+    REAL(KIND=JPRB),    INTENT(IN)    :: PAP(:,:,:)   ! Pressure on full levels
+    REAL(KIND=JPRB),    INTENT(IN)    :: PAPH(:,:,:)  ! Pressure on half levels
+    REAL(KIND=JPRB),    INTENT(IN)    :: PLU(:,:,:)   ! Conv. condensate
+    REAL(KIND=JPRB),    INTENT(INOUT) :: PLUDE(:,:,:) ! Conv. detrained water
+    REAL(KIND=JPRB),    INTENT(IN)    :: PMFU(:,:,:)  ! Conv. mass flux up
+    REAL(KIND=JPRB),    INTENT(IN)    :: PMFD(:,:,:)  ! Conv. mass flux down
+    REAL(KIND=JPRB),    INTENT(IN)    :: PA(:,:,:)    ! Original Cloud fraction (t)
+    REAL(KIND=JPRB),    INTENT(IN)    :: PCLV(:,:,:,:) 
+    REAL(KIND=JPRB),    INTENT(IN)    :: PSUPSAT(:,:,:)
+    REAL(KIND=JPRB),    INTENT(INOUT) :: PCOVPTOT(:,:,:) ! Precip fraction
+    REAL(KIND=JPRB),    INTENT(OUT)   :: PFPLSL(:,:,:) ! liq+rain sedim flux
+    REAL(KIND=JPRB),    INTENT(OUT)   :: PFPLSN(:,:,:) ! ice+snow sedim flux
+    REAL(KIND=JPRB),    INTENT(OUT)   :: PFHPSL(:,:,:) ! Enthalpy flux for liq
+    REAL(KIND=JPRB),    INTENT(OUT)   :: PFHPSN(:,:,:) ! Enthalp flux for ice
+
+    INTEGER(KIND=JPIM) :: JKGLO,IBL,ICEND,NGPBLKS
+
+    TYPE(PERFORMANCE_TIMER) :: TIMER
+    REAL(KIND=JPRD), PARAMETER :: ZHPM = 3996006.0_JPRD  ! The nominal number of flops per 100 columns
+
+    INTEGER(KIND=JPIM) :: TID ! thread id from 0 .. NUMOMP - 1
+    LOGICAL            :: LDRAIN1D = .FALSE.
+    REAL(KIND=JPRB)    :: ZQSAT(NPROMA,NLEV) ! local array
+
+    NGPBLKS = (NGPTOT / NPROMA) + MIN(MOD(NGPTOT,NPROMA), 1)
+1003 format(5x,'NUMPROC=',i0', NUMOMP=',i0,', NGPTOTG=',i0,', NPROMA=',i0,', NGPBLKS=',i0)
+    if (irank == 0) then
+      write(0,1003) NUMPROC,NUMOMP,NGPTOTG,NPROMA,NGPBLKS
+    end if
+
+    ! Global timer for the parallel region
+    CALL TIMER%START(NUMOMP)
+
+    !$omp parallel default(shared) private(JKGLO,IBL,ICEND,TID) &
+    !$omp& private(ZQSAT) &
+    !$omp& num_threads(NUMOMP)
+
+    ! Local timer for each thread
+    TID = GET_THREAD_NUM()
+    CALL TIMER%THREAD_START(TID)
+
+    !$omp do schedule(runtime)
+    DO JKGLO=1,NGPTOT,NPROMA
+       IBL=(JKGLO-1)/NPROMA+1
+       ICEND=MIN(NPROMA,NGPTOT-JKGLO+1)
+
+         !-- These were uninitialized : meaningful only when we compare error differences
+         PCOVPTOT(:,:,IBL) = 0.0_JPRB
+!         TENDENCY_LOC(IBL)%cld(:,:,NCLV) = 0.0_JPRB
+
+         ! Fill in ZQSAT
+         CALL SATUR (1, ICEND, NPROMA, 1, NLEV, .TRUE., &
+              & PAP(:,:,IBL), PT(:,:,IBL), ZQSAT(:,:), 2) 
+
+!        CALL CLOUDSC2 ( &
+!             &  1, ICEND, NPROMA, 1, NLEV, LDRAIN1D, &
+!             & PTSPHY,&
+!             & PAPH(:,:,IBL),  PAP(:,:,IBL), &
+!             & PQ(:,:,IBL), ZQSAT(:,:), PT(:,:,IBL), &
+!             & PCLV(:,:,NCLDQL,IBL), PCLV(:,:,NCLDQI,IBL), &
+!             & PLUDE(:,:,IBL), PLU(:,:,IBL), PMFU(:,:,IBL), PMFD(:,:,IBL),&
+!             &  TENDENCY_LOC(IBL)%T, TENDENCY_CML(IBL)%T, &
+!             &  TENDENCY_LOC(IBL)%Q, TENDENCY_CML(IBL)%Q, &
+!             &  TENDENCY_LOC(IBL)%CLD(:,:,NCLDQL), TENDENCY_CML(IBL)%CLD(:,:,NCLDQL), &
+!             &  TENDENCY_LOC(IBL)%CLD(:,:,NCLDQI), TENDENCY_CML(IBL)%CLD(:,:,NCLDQI), &
+!             &  PSUPSAT(:,:,IBL), &
+!             &  PA(:,:,IBL), PFPLSL(:,:,IBL),   PFPLSN(:,:,IBL), &
+!             &  PFHPSL(:,:,IBL),   PFHPSN(:,:,IBL), PCOVPTOT(:,:,IBL))
+
+         ! Log number of columns processed by this thread
+         CALL TIMER%THREAD_LOG(TID, IGPC=ICEND)
+      ENDDO
+
+      !-- The "nowait" is here to get correct local timings (tloc) per thread
+      !   i.e. we should not wait for slowest thread to finish before measuring tloc
+      !$omp end do nowait
+
+      CALL TIMER%THREAD_END(TID)
+
+      !$omp end parallel
+
+      CALL TIMER%END()
+
+      CALL TIMER%PRINT_PERFORMANCE(NPROMA, NGPBLKS, ZHPM, NGPTOT)
+    
+  END SUBROUTINE CLOUDSC_DRIVER_TEST
 
   SUBROUTINE CLOUDSC_DRIVER( &
      & NUMOMP, NPROMA, NLEV, NGPTOT, NGPTOTG, NBLOCKS, PTSPHY, &
@@ -36,8 +150,8 @@ CONTAINS
     REAL(KIND=JPRB),    INTENT(IN)    :: PTSPHY       ! Physics timestep
     REAL(KIND=JPRB),    INTENT(IN)    :: PT(:,:,:)    ! T at start of callpar
     REAL(KIND=JPRB),    INTENT(IN)    :: PQ(:,:,:)    ! Q at start of callpar
-    TYPE(STATE_TYPE),   INTENT(IN)    :: TENDENCY_CML(NBLOCKS) ! cumulative tendency used for final output
-    TYPE(STATE_TYPE),   INTENT(OUT)   :: TENDENCY_LOC(NBLOCKS) ! local tendency from cloud scheme
+    TYPE(STATE_TYPE),   INTENT(IN)    :: TENDENCY_CML(10) ! cumulative tendency used for final output
+    TYPE(STATE_TYPE),   INTENT(OUT)   :: TENDENCY_LOC(10) ! local tendency from cloud scheme
     REAL(KIND=JPRB),    INTENT(IN)    :: PAP(:,:,:)   ! Pressure on full levels
     REAL(KIND=JPRB),    INTENT(IN)    :: PAPH(:,:,:)  ! Pressure on half levels
     REAL(KIND=JPRB),    INTENT(IN)    :: PLU(:,:,:)   ! Conv. condensate
--- src/cloudsc2_nl/dwarfdriver.py
+++ src/cloudsc2_nl/dwarfdriver.py
@@ -1,6 +1,7 @@
 import _Exampledwarf
 import f90wrap.runtime
 import logging
+import numpy as np
 
 class Dwarf(f90wrap.runtime.FortranModule):
 
@@ -22,6 +23,38 @@ class Dwarf(f90wrap.runtime.FortranModule):
         _Exampledwarf.f90wrap_cloudsc_driver_print(numomp=numomp, nproma=nproma, nlev=nlev, ngptot=ngptot, ngptotg=ngptotg, nblocks=nblocks)
         
         
+    @staticmethod
+    def do_dwarf_full_call(numomp, nproma, nlev, ngptot, ngptotg, nblocks,
+                           ptsphy,
+                           pt, pq, 
+             #             tendency_cml, tendency_loc, 
+                           pap,      paph, 
+                           plu,      plude,    pmfu,     pmfd, 
+                           pa,       pclv,     psupsat,
+                           pcovptot, 
+                           pfplsl,   pfplsn,   pfhpsl,   pfhpsn):
+        """
+        _Exampledwarf.cloudsc_driver(NUMOMP, NPROMA, NLEV, NGPTOT, NGPTOTG, NBLOCKS) 
+
+        Parameters
+        ----------
+        numomp : int
+        nproma : int
+        nlev   : int
+        ngptot : int
+        ngptotg: int
+        nblocks: int
+
+        """
+        _Exampledwarf.f90wrap_cloudsc_driver_test(numomp=numomp, nproma=nproma, nlev=nlev, ngptot=ngptot, ngptotg=ngptotg, nblocks=nblocks,
+                                                  ptsphy=ptsphy,
+                                                   pt=pt, pq=pq,
+                                                #   tendency_cml=tendency_cml, tendency_loc=tendency_loc,
+                                                   pap=pap,      paph=paph,
+                                                   plu=plu,      plude=plude,    pmfu=pmfu,     pmfd=pmfd,
+                                                   pa=pa,       pclv=pclv,     psupsat=psupsat,
+                                                   pcovptot=pcovptot,
+                                                   pfplsl=pfplsl,   pfplsn=pfplsn,   pfhpsl=pfhpsl,   pfhpsn=pfhpsn)
 dwarf = Dwarf()
 numomp=1
 nproma=1
@@ -29,4 +62,31 @@ nlev=1
 ngptot=1
 ngptotg=1
 nblocks=1
+ndim=1
+ptsphy=1.
+pt        = np.zeros((ngptot,nlev  ,ndim), order='F')
+pq        = np.zeros((ngptot,nlev  ,ndim), order='F')
+pap       = np.zeros((ngptot,nlev  ,ndim), order='F')
+paph      = np.zeros((ngptot,nlev+1,ndim), order='F')
+plu       = np.zeros((ngptot,nlev  ,ndim), order='F')
+plude     = np.zeros((ngptot,nlev  ,ndim), order='F')
+pmfu      = np.zeros((ngptot,nlev  ,ndim), order='F')
+pmfd      = np.zeros((ngptot,nlev  ,ndim), order='F')
+pa        = np.zeros((ngptot,nlev  ,ndim), order='F')
+pclv      = np.zeros((ngptot,nlev  ,ndim), order='F')
+psupsat   = np.zeros((ngptot,nlev  ,ndim), order='F')
+pcovptot  = np.zeros((ngptot,nlev  ,ndim), order='F')
+pfplsl    = np.zeros((ngptot,nlev+1,ndim), order='F')
+pfplsn    = np.zeros((ngptot,nlev+1,ndim), order='F')
+pfhpsl    = np.zeros((ngptot,nlev+1,ndim), order='F')
+pfhpsn    = np.zeros((ngptot,nlev+1,ndim), order='F')
+
 dwarf.do_dwarf_call(numomp, nproma, nlev, ngptot, ngptotg, nblocks)
+dwarf.do_dwarf_full_call(numomp, nproma, nlev, ngptot, ngptotg, nblocks,
+                         ptsphy,
+                         pt,pq,
+                         pap, paph,
+                         plu, plude, pmfu, pmfd,
+                         pa,pclv,psupsat,
+                         pcovptot,
+                         pfplsl, pfplsn, pfhpsl, pfhpsn)
